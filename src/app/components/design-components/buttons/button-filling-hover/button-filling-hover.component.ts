a6fe2e94cdaa1ae4aac58fad4331fb31d7cb9040
Benedek Balázs <benbal87@gmail.com>
2024-02-29 16:13:27 +0700

    :sparkles: button filling hover added

--- src/app/components/design-components/buttons/button-filling-hover/button-filling-hover.component.ts
+++ src/app/components/design-components/buttons/button-filling-hover/button-filling-hover.component.ts
@@ -16,7 +16,13 @@ export class ButtonFillingHoverComponent implements GeneralDesignComponent {
   static get template(): string {
     return DesignItemService.getFormattedTemplateFromString(
       `
+        <button class="btn-filling-hover">
+          Button
+        </button>
 
+        <button class="btn-filling-hover" disabled>
+          Button Disabled
+        </button>
       `
     )
   }
@@ -24,7 +30,78 @@ export class ButtonFillingHoverComponent implements GeneralDesignComponent {
   static get style(): string {
     return DesignItemService.getFormattedStyle(
       `
+        button.btn-filling-hover {
+          user-select: none;
+          cursor: pointer;
+          transform: translate(0, 0);
+          padding: 1rem 2rem;
+          text-transform: uppercase;
+          text-decoration: none;
+          color: #ffffff;
+          background-color: transparent;
+          outline: none;
+          border: none;
+          letter-spacing: 2px;
+          font-size: 20px;
 
+          &:after {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: transparent;
+            border: 2px solid #ffffff;
+            box-sizing: border-box;
+            z-index: -1;
+            transition: transform 0.5s;
+            transform-origin: top left;
+            transform: scale(1);
+          }
+
+          &:not(:disabled) {
+
+            &:before {
+              content: "";
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+              background: #03a9f4;
+              z-index: -1;
+              transition: transform 0.5s;
+              transform-origin: bottom right;
+              transform: scale(0);
+            }
+
+            &:after {
+              transition: transform 0.5s;
+              transform-origin: top left;
+              transform: scale(1);
+            }
+
+            &:hover {
+              &:before {
+                transition: transform 0.5s;
+                transform-origin: top left;
+                transform: scale(1);
+              }
+
+              &:after {
+                transition: transform 0.5s;
+                transform-origin: bottom right;
+                transform: scale(0);
+              }
+            }
+          }
+
+          &:disabled {
+            cursor: not-allowed;
+            opacity: 0.3;
+          }
+        }
       `
     )
   }
